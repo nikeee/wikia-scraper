e744206b8ce7ad8152a7a4339ff9fb15487e6b52
Niklas Mollenhauer <nikeee@outlook.com>
2018-01-11 11:45:20 +0100

    Add documentation

--- create-wordlist.sh
+++ create-wordlist.sh
@@ -4,4 +4,11 @@ urldecode(){
 	echo -e "$(sed 's/+/ /g;s/%\(..\)/\\x\1/g;')"
 }
 
+# Concatinate all input files,
+# Remove all blanks
+# Convert everything to lower case
+# Remove all colons
+# sort
+# Remove duplicates
+# Decode percent encoding
 cat "$@" | sed -E -e 's/[[:blank:]]+//g' | sed 's/.*/\L&/' | sed 's/://g' | sort | uniq | urldecode
